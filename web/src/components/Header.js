813bbf902218aa0913e7dcd56c6b3284be8e7082
velmurgan pillai <karthikpillai1323@gmail.com>
2021-07-22 13:50:33 +0530

    add readme file

---
+++ web/src/components/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Header() {
+  return (
+    <header>
+      <h1 className="text-8xl text-red-400">pink world</h1>
+    </header>
+  );
+}
+
+export default Header;
