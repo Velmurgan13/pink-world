36edcd89632ef17bf55382bb7f81232c456c340b
velmurgan pillai <karthikpillai1323@gmail.com>
2021-08-05 14:07:39 +0530

    Added FAQ and Footer Component

---
+++ web/src/components/FAQ.js
@@ -0,0 +1,90 @@
+import React from "react";
+
+function FAQ() {
+  return (
+    <div className="flex flex-col w-full items-center justify-items-center h-96">
+      <div className="flex flex-col w-10/12 h-full">
+        <div className="text-3xl text-center m-8 w-full">
+          <h4 className="font-bold">Why Pink World?</h4>
+        </div>
+        <div className="flex flex-row w-full h-full">
+          <div className="flex flex-col w-3/12 my-5 mx-5 h-full items-center">
+            <div className="">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-24 w-24 py-3"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M9 12l2 2 4-4m5.618-4.016A11.955 11.955 0 0112 2.944a11.955 11.955 0 01-8.618 3.04A12.02 12.02 0 003 9c0 5.591 3.824 10.29 9 11.622 5.176-1.332 9-6.03 9-11.622 0-1.042-.133-2.052-.382-3.016z"
+                />
+              </svg>
+            </div>
+            <div className=" flex flex-col text-center">
+              <h4 className="font-bold text-lg">Your Trusted Advisor</h4>
+              <p className="text-left mt-3">
+                We’ll answer your every question. Our experienced travel experts
+                and real-time on field know-how gives us this advantage.
+              </p>
+            </div>
+          </div>
+          <div className="flex flex-col w-3/12 my-5 mx-5 h-full items-center">
+            <div className="">
+              <img
+                src="/images/listening.png"
+                alt="listen"
+                className="h-24 w-24 py-3"
+              ></img>
+            </div>
+            <div className=" flex flex-col text-center">
+              <h4 className="font-bold text-lg">We Love Listening</h4>
+              <p className="text-left mt-3">
+                Your holiday, your terms. We’ll fill in the blanks to plan the
+                perfect trip in the blink of an eye.
+              </p>
+            </div>
+          </div>
+          <div className="flex flex-col w-3/12 my-5 mx-5 h-full items-center">
+            <div className="">
+              <img
+                src="/images/smile.jpeg"
+                alt="listen"
+                className="h-24 w-24 py-3"
+              ></img>
+            </div>
+            <div className=" flex flex-col text-center">
+              <h4 className="font-bold text-lg">Memorable Experience</h4>
+              <p className="text-left mt-3">
+                Do everything or do nothing. Either way, your holiday will be
+                nothing less than extraordinary.
+              </p>
+            </div>
+          </div>
+          <div className="flex flex-col w-3/12 my-5 mx-5 h-full items-center">
+            <div className="">
+              <img
+                src="/images/handcraft.png"
+                alt="listen"
+                className="h-24 w-24 py-3"
+              ></img>
+            </div>
+            <div className=" flex flex-col text-center">
+              <h4 className="font-bold text-lg">Handcrafted Holidays</h4>
+              <p className="text-left mt-3">
+                We interact with our loyal customers to co-create unique
+                experiences that will take your holiday to the next level.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default FAQ;
--- web/src/components/Footer.js
+++ web/src/components/Footer.js
@@ -2,9 +2,68 @@ import React from "react";
 
 function Footer() {
   return (
-    <footer>
-      <h1>This is footer tag </h1>
-    </footer>
+    <div className="flex flex-col w-full items-center justify-items-center bg-gray-300 py-10 mt-5">
+      <div className="flex flex-row w-10/12 mt-10">
+        <div className=" flex flex-col w-3/12">
+          <h3 className="font-bold text-xl">Search & Book</h3>
+          <div className="flex w-full text-left mt-4">
+            <ul className="text-md">
+              <li className="mt-2">Bus</li>
+              <li className="mt-2">Trains</li>
+              <li className="mt-2">Flights</li>
+              <li className="mt-2">Hotels</li>
+              <li className="mt-2">Packages</li>
+            </ul>
+          </div>
+        </div>
+        <div className=" flex flex-col w-3/12">
+          <div className=" flex flex-col w-full">
+            <h3 className="font-bold text-xl">Company Info</h3>
+            <div className="text-left mt-4">
+              <ul className="text-md">
+                <li className="mt-2">About Pink World</li>
+                <li className="mt-2">Contact Us</li>
+              </ul>
+            </div>
+          </div>
+          <div className="flex flex-col w-full mt-5">
+            <h3 className="font-bold text-xl">B2B Offerings</h3>
+            <div className="flex flex-col">
+              <ul className="text-md">
+                <li className="mt-2">Pink World Login</li>
+                <li className="mt-2">B2B Hotels</li>
+                <li className="mt-2">MICE Services</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+        <div className=" flex flex-col w-3/12">
+          <div className="flex flex-col w-full">
+            <h3 className="font-bold text-xl">Useful Links</h3>
+          </div>
+          <div className="flex flex-col mt-4">
+            <ul className="text-md">
+              <li className="mt-2">Terms & Conditions</li>
+              <li className="mt-2">Privacy Policy</li>
+              <li className="mt-2">Feedback</li>
+              <li className="mt-2">FAQ</li>
+            </ul>
+          </div>
+        </div>
+        <div className=" flex flex-col w-3/12">
+          <div className="flex flex-col w-full">
+            <h4 className="font-bold text-xl">Find Us On</h4>
+          </div>
+          <div className="flex flex-col mt-4">
+            <ul className="text-md">
+              <li className="mt-2">Facebook</li>
+              <li className="mt-2">Instagram</li>
+              <li className="mt-2">Youtube</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
 
