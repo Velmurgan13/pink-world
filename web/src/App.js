ca05c9fe1ec4069494c5e4de407ea34343a1ab98
velmurgan pillai <karthikpillai1323@gmail.com>
2021-08-05 14:08:38 +0530

    Added Homepage and updated App.js

--- web/src/App.js
+++ web/src/App.js
@@ -1,19 +1,17 @@
 import "./App.css";
-import Header from "./components/Header";
-import Footer from "./components/Footer";
 import Homepage from "./components/Homepage";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 function App() {
   return (
     <div className="app-container">
-      <Header />
-      <Router>
+      <Homepage />
+      {/* <Router>
         <Switch>
           <Route exact path="/" component={Homepage} />
         </Switch>
-      </Router>
-      <Footer />
+      </Router> */}
+      {/* <Footer /> */}
     </div>
   );
 }
