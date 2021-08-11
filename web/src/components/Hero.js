c38855d2ddde0999c02a69104c7d3aa1342a68b4
velmurgan pillai <karthikpillai1323@gmail.com>
2021-08-12 00:27:22 +0530

    Added image slider

--- web/src/components/Hero.js
+++ web/src/components/Hero.js
@@ -1,15 +1,37 @@
 import React from "react";
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import { Carousel } from "react-responsive-carousel";
 
 function Hero() {
   return (
     <div className="flex flex-col w-full items-center justify-items-center">
-      <div className="flex flex-col  w-10/12 items-center justify-items-center">
-        <div className="w-10/12 items-center mt-4">
-          <img
-            src="/images/image1.jpg"
-            alt="Tajmahal"
-            className="w-full rounded-2xl"
-          ></img>
+      <div className="block w-10/12">
+        <div className="relative mt-5">
+          <Carousel
+            axis="horizontal"
+            showArrows={true}
+            showIndicators={true}
+            showThumbs={false}
+            showStatus={false}
+          >
+            <img
+              src="images/carousel-3.jpeg"
+              alt="carousel-1"
+              className="rounded-lg"
+            />
+
+            <img
+              src="images/carousel-4.jpeg"
+              alt="carousel-4"
+              className="rounded-lg"
+            />
+
+            <img
+              src="images/carousel-2.jpeg"
+              alt="carousel-2"
+              className="rounded-lg"
+            />
+          </Carousel>
         </div>
       </div>
     </div>
